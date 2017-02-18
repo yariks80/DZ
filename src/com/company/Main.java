9d9d3d9e05112bbdcb0f33e296f6f4a93cf87dc5
iaroslav sviatkin <yariks@i.ua>
2017-02-18 09:51:33 +0200

    ASD

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+    static int[][] a = {{1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(a));
+        for (int i = 1; i < a.length; i++) {
+            int temp = a[0][i];
+            a[0][i] = a[i][i];
+            a[i][i] = temp;
+        }
+        System.out.println(Arrays.deepToString(a));
+    }
+}
